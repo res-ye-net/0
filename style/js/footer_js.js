56a884f08ba3fded059224f4e991c77bd82dc454
res-ye-net <108458768+res-ye-net@users.noreply.github.com>
2022-07-05 13:15:48 +0300

    Add files via upload

---
+++ style/js/footer_js.js
@@ -0,0 +1,3 @@
+function getState(val){var gid=$("#year").val();$.ajax({type:"POST",url:"lap.php?get=city",data:'rid='+val+'&gid='+gid,success:function(data){$("#city").html(data);}});}
+function getsrch(){var gid=$("#year").val();var rid=$("#grade").val();$.ajax({type:"POST",url:"lap.php?get=srch",data:'rid='+rid+'&gid='+gid,success:function(data){$("#do").html(data);}});}
+function myFunction(){var input,filter,table,tr,td,i;input=document.getElementById("myInput");filter=input.value.toUpperCase();table=document.getElementById("myTable");tr=table.getElementsByTagName("tr");for(i=0;i<tr.length;i++){td=tr[i].getElementsByTagName("td")[1];if(td){if(td.innerHTML.toUpperCase().indexOf(filter)>-1){tr[i].style.display="";}else{tr[i].style.display="none";}}}}
